50b0a2cf426e308545fbab765e2073aa7fd8e87b
CarlosRPaz <trojan.carlos44@gmail.com>
2021-03-18 11:23:08 -0700

    Netflix Clone started

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJN90d1_5G02ZZ3_c9zRPfu5Odnt7RdM8",
+    authDomain: "netflix-clone-e5f91.firebaseapp.com",
+    projectId: "netflix-clone-e5f91",
+    storageBucket: "netflix-clone-e5f91.appspot.com",
+    messagingSenderId: "281037596385",
+    appId: "1:281037596385:web:1c6ab2bc5507c5888df9a0"
+};
